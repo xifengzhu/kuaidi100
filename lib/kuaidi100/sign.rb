cd7711d22348c99b2b628bfaf2e17056f6b07d5d
leif <leif@beansmile.com>
2018-08-15 15:07:31 +0800

    feat: add callback sign verify

--- lib/kuaidi100/sign.rb
+++ lib/kuaidi100/sign.rb
@@ -6,5 +6,13 @@ module Kuaidi100
       string_sign_temp = (param.to_json + Kuaidi100.key + Kuaidi100.customer).to_s
       Digest::MD5.hexdigest(string_sign_temp).upcase
     end
+
+    # MD5(param + salt) == sign
+    def self.callback_verify?(params)
+      params = params.dup
+      sign = params.delete('sign')
+      sign_string = params.delete('param') + Kuaidi100.salt
+      sign == Digest::MD5.hexdigest(sign_string).upcase
+    end
   end
 end
--- lib/kuaidi100/version.rb
+++ lib/kuaidi100/version.rb
@@ -1,3 +1,3 @@
 module Kuaidi100
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
